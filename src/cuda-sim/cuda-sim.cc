7fdf6a7800a29b84d1bbf9c046c6e3cf47150a29
Tor Aamodt <aamodt@ece.ubc.ca>
2011-11-16 23:27:21 -0800

    change version number to 3.0.1 build XYZ
    [git-p4: depot-paths = "//depot/gpgpu_sim_research/fermi/distribution/": change = 10969]

--- src/cuda-sim/cuda-sim.cc
+++ src/cuda-sim/cuda-sim.cc
@@ -1215,7 +1215,7 @@ kernel_info_t *gpgpu_opencl_ptx_sim_init_grid(class function_info *entry,
 }
 
 const char *g_gpgpusim_build_string = "$Change$";
-const char *g_gpgpusim_version_string = "3.0";
+const char *g_gpgpusim_version_string = "3.0.1";
 
 void print_splash()
 {
@@ -1223,7 +1223,7 @@ void print_splash()
    if ( !splash_printed ) {
       unsigned build=0;
       sscanf(g_gpgpusim_build_string, "$Change"": %u $", &build);
-      fprintf(stdout, "\n\n        *** GPGPU-Sim version %s.%u ***\n\n\n", g_gpgpusim_version_string, build );
+      fprintf(stdout, "\n\n        *** GPGPU-Sim version %s (build %u) ***\n\n\n", g_gpgpusim_version_string, build );
       splash_printed=1;
    }
 }
