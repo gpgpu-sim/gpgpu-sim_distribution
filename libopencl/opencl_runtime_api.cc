20a104af5089be685b96b8678ed2b6730e37295f
Wilson Fung <wlfung@gmail.com>
2012-01-12 04:54:49 -0800

    Adding stub function implementation to get EA_Ropa running on fermi branch.
    [git-p4: depot-paths = "//depot/gpgpu_sim_research/fermi/distribution/": change = 11311]

--- libopencl/opencl_runtime_api.cc
+++ libopencl/opencl_runtime_api.cc
@@ -1415,3 +1415,16 @@ clEnqueueMapBuffer(cl_command_queue command_queue,
    assert( mem->is_on_host() );
    return mem->host_ptr();
 }
+
+
+extern CL_API_ENTRY cl_int CL_API_CALL
+clSetCommandQueueProperty( cl_command_queue command_queue,
+                              cl_command_queue_properties properties,
+                              cl_bool enable,
+                              cl_command_queue_properties *old_properties
+                           ) CL_API_SUFFIX__VERSION_1_0
+{
+   // TODO: do something here
+   return CL_SUCCESS;
+}
+
