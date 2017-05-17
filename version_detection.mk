1e7f2f0d857666ee7edd7bd0d6b0e52a6588522e
tgrogers <timrogers@purdue.edu>
2017-05-16 23:03:47 -0400

    Getting rid of the branch since it gets weird, espeically in travis

--- version_detection.mk
+++ version_detection.mk
@@ -32,10 +32,9 @@ else
 GPGPUSIM_VERSION=$(shell cat $(GPGPUSIM_ROOT)/version | awk '/Version/ {print $$8}' )
 
 #Detect Git branch and commit #
-GIT_BRANCH := $(shell git branch | grep "\*" | sed -re 's/\*\s+(.*)/\1/')
 GIT_COMMIT := $(shell git log -n 1 | head -1 | sed -re 's/commit (.*)/\1/')
 GIT_FILES_CHANGED := $(shell git diff --numstat --cached && git diff --numstat | wc | sed -re 's/^\s+([0-9]+).*/\1/')
-GPGPUSIM_BUILD := "gpgpu-sim_$(GIT_BRANCH)_$(GIT_COMMIT)_modified_$(GIT_FILES_CHANGED)"
+GPGPUSIM_BUILD := "gpgpu-sim_git-commit-$(GIT_COMMIT)_modified_$(GIT_FILES_CHANGED)"
 endif
 
 # Detect CUDA Runtime Version 
