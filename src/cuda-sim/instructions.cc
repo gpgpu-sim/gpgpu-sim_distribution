9b0de46d9be80953c211387882f7965940e0a1c7
Jonathan <jonathanslew@outlook.com>
2018-05-25 09:32:49 -0700

    fixed merge issue

--- src/cuda-sim/instructions.cc
+++ src/cuda-sim/instructions.cc
@@ -2366,12 +2366,12 @@ void decode_space( memory_space_t &space, ptx_thread_info *thread, const operand
       type_info_key ti = t->get_key();
       if( ti.is_param_kernel() )
          space = param_space_kernel;
-      else if( ti.is_param_local() ) 
+      else if( ti.is_param_local() ) {
          space = param_space_local;
       } 
       else if( ti.is_reg() ) {
          space = param_space_kernel;
-      }	
+      }  
       else {
          printf("GPGPU-Sim PTX: ERROR ** cannot resolve .param space for '%s'\n", s->name().c_str() );
          abort(); 
