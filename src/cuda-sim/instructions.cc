3ccedf750e180a63a286b4e7b3bd687901ce1ca9
Jiangqiu Shen <jshen2@mtu.edu>
2018-10-28 23:49:35 -0400

    return to 4.4

--- src/cuda-sim/instructions.cc
+++ src/cuda-sim/instructions.cc
@@ -1864,7 +1864,7 @@ ptx_reg_t d2d( ptx_reg_t x, unsigned from_width, unsigned to_width, int to_sign,
       y.f64 = x.f64;
       break; 
    }
-   if (std::isnan(y.f64)) {
+   if (isnan(y.f64)) {
       y.u64 = 0xfff8000000000000ull;
    } else if (saturation_mode) {
       y.f64 = cuda_math::__saturatef(y.f64); 
@@ -1989,7 +1989,7 @@ void ptx_round(ptx_reg_t& data, int rounding_mode, int type)
       }
    }
    if ((type == F64_TYPE)||(type == FF64_TYPE)) {
-      if (std::isnan(data.f64)) {
+      if (isnan(data.f64)) {
          data.u64 = 0xfff8000000000000ull;
       }
    }
@@ -2546,12 +2546,12 @@ void mad_def( const ptx_instruction *pI, ptx_thread_info *thread, bool use_carry
 
 bool isNaN(float x)
 {
-   return std::isnan(x);
+   return isnan(x);
 }
 
 bool isNaN(double x)
 {
-   return std::isnan(x);
+   return isnan(x);
 }
 
 void max_impl( const ptx_instruction *pI, ptx_thread_info *thread ) 
