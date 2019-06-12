7920f8abe34be2bac69eb9758b2fdb4457f4ca27
Mengchi Zhang <zhan2308@purdue.edu>
2019-06-12 18:17:45 -0400

    Move all ptx_parser inside gpgpu_context
    Signed-off-by: Mengchi Zhang <zhan2308@purdue.edu>

--- libcuda/gpgpu_context.h
+++ libcuda/gpgpu_context.h
@@ -2,17 +2,20 @@
 #define __gpgpu_context_h__
 #include "cuda_api_object.h"
 #include "../src/cuda-sim/ptx_loader.h"
+#include "../src/cuda-sim/ptx_parser.h"
 
 class gpgpu_context {
     public:
 	gpgpu_context() {
 	    api = new cuda_runtime_api();
 	    ptxinfo = new ptxinfo_data();
+	    ptx_parser = new ptx_recognizer();
 	}
 	// global list
 	// objects pointers for each file
 	cuda_runtime_api* api;
 	ptxinfo_data* ptxinfo;
+	ptx_recognizer* ptx_parser;
 	// member function list
 	void cuobjdumpParseBinary(unsigned int handle);
 	class symbol_table *gpgpu_ptx_sim_load_ptx_from_string( const char *p, unsigned source_num );
@@ -20,6 +23,7 @@ class gpgpu_context {
 	void gpgpu_ptx_info_load_from_filename( const char *filename, unsigned sm_version);
 	void gpgpu_ptxinfo_load_from_string( const char *p_for_info, unsigned source_num, unsigned sm_version=20, int no_of_ptx=0 );
 	void print_ptx_file( const char *p, unsigned source_num, const char *filename );
+	class symbol_table* init_parser(const char*);
 };
 gpgpu_context* GPGPU_Context();
 
