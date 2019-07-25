2fa458570a2d566faca2f99b1e7361fdc454129e
boyealkim <gangmul12@snu.ac.kr>
2019-07-25 20:31:21 +0900

    fix stream fix bug and fix cudaStreamWaitEvent impl

--- libcuda/cuda_runtime_api.cc
+++ libcuda/cuda_runtime_api.cc
@@ -1696,6 +1696,7 @@ __host__ cudaError_t CUDARTAPI cudaEventRecord(cudaEvent_t event, cudaStream_t s
 	if( !e ) return g_last_cudaError = cudaErrorUnknown;
 	struct CUstream_st *s = (struct CUstream_st *)stream;
 	stream_operation op(e,s);
+	e->issue();
 	g_stream_manager->push(op);
 	return g_last_cudaError = cudaSuccess;
 }
@@ -1708,9 +1709,13 @@ __host__ cudaError_t CUDARTAPI cudaStreamWaitEvent(cudaStream_t stream, cudaEven
    //reference: https://www.cs.cmu.edu/afs/cs/academic/class/15668-s11/www/cuda-doc/html/group__CUDART__STREAM_gfe68d207dc965685d92d3f03d77b0876.html 
 	CUevent_st *e = get_event(event);
 	if( !e ){
-	   printf("GPGPU-Sim API: Warning: cudaEventRecord has not been called on event before calling cudaStreamWaitEvent.\nNothing to be done.\n");
-      return g_last_cudaError = cudaSuccess;
+	   printf("GPGPU-Sim API: Error at cudaStreamWaitEvent. Event is not created .\n");
+		return g_last_cudaError = cudaErrorInvalidResourceHandle;
    }
+	else if(e->num_issued() == 0){
+		printf("GPGPU-Sim API: Warning: cudaEventRecord has not been called on event before calling cudaStreamWaitEvent.\nNothing to be done.\n");
+      return g_last_cudaError = cudaSuccess;
+	}
    if (!stream){
       g_stream_manager->pushCudaStreamWaitEventToAllStreams(e, flags);
    } else {
