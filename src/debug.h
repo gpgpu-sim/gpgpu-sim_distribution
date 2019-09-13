bfe5f713355ee752a97bfe740a318d7956099eeb
Nick <coffeebeforearch@gmail.com>
2019-09-13 07:48:08 -0400

    Revert "Add src/ director formatting"
    This reverts commit 26ca8de4a6ec9bfe422a14cbe325a5f257df453b.

--- src/debug.h
+++ src/debug.h
@@ -7,16 +7,14 @@
 //
 // Redistributions of source code must retain the above copyright notice, this
 // list of conditions and the following disclaimer.
-// Redistributions in binary form must reproduce the above copyright notice,
-// this
+// Redistributions in binary form must reproduce the above copyright notice, this
 // list of conditions and the following disclaimer in the documentation and/or
 // other materials provided with the distribution.
 // Neither the name of The University of British Columbia nor the names of its
 // contributors may be used to endorse or promote products derived from this
 // software without specific prior written permission.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 // DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
@@ -35,53 +33,58 @@
 #include <string>
 
 class brk_pt {
- public:
-  brk_pt() { m_valid = false; }
-  brk_pt(const char *fileline, unsigned uid) {
-    m_valid = true;
-    m_watch = false;
-    m_fileline = std::string(fileline);
-    m_thread_uid = uid;
-  }
-  brk_pt(unsigned addr, unsigned value) {
-    m_valid = true;
-    m_watch = true;
-    m_addr = addr;
-    m_value = value;
-  }
+public:
+   brk_pt() { m_valid=false; }
+   brk_pt( const char *fileline, unsigned uid )
+   {
+      m_valid = true;
+      m_watch = false;
+      m_fileline = std::string(fileline);
+      m_thread_uid=uid;
+   }
+   brk_pt( unsigned addr, unsigned value )
+   {
+      m_valid = true;
+      m_watch = true;
+      m_addr = addr;
+      m_value = value;
+   }
 
-  unsigned get_value() const { return m_value; }
-  addr_t get_addr() const { return m_addr; }
-  bool is_valid() const { return m_valid; }
-  bool is_watchpoint() const { return m_watch; }
-  bool is_equal(const std::string &fileline, unsigned uid) const {
-    if (m_watch) return false;
-    if ((m_thread_uid != (unsigned)-1) && (uid != m_thread_uid)) return false;
-    return m_fileline == fileline;
-  }
-  std::string location() const {
-    char buffer[1024];
-    sprintf(buffer, "%s thread uid = %u", m_fileline.c_str(), m_thread_uid);
-    return buffer;
-  }
+   unsigned get_value() const { return m_value; }
+   addr_t get_addr() const { return m_addr; }
+   bool is_valid() const { return m_valid; }
+   bool is_watchpoint() const { return m_watch; }
+   bool is_equal( const std::string &fileline, unsigned uid ) const
+   {
+      if( m_watch ) 
+         return false; 
+      if( (m_thread_uid != (unsigned)-1) && (uid != m_thread_uid) ) 
+         return false;
+      return m_fileline == fileline;
+   }
+   std::string location() const
+   {
+      char buffer[1024];
+      sprintf(buffer,"%s thread uid = %u", m_fileline.c_str(), m_thread_uid);
+      return buffer;
+   }
 
-  unsigned set_value(unsigned val) { return m_value = val; }
+   unsigned set_value( unsigned val ) { return m_value=val; }
+private:
+   bool         m_valid;
+   bool         m_watch;
 
- private:
-  bool m_valid;
-  bool m_watch;
+   // break point
+   std::string  m_fileline;
+   unsigned     m_thread_uid;
 
-  // break point
-  std::string m_fileline;
-  unsigned m_thread_uid;
-
-  // watch point
-  unsigned m_addr;
-  unsigned m_value;
+   // watch point
+   unsigned     m_addr;
+   unsigned     m_value;
 };
 
 class ptx_thread_info;
 class ptx_instruction;
-bool thread_at_brkpt(ptx_thread_info *thd_info, const class brk_pt &b);
+bool thread_at_brkpt( ptx_thread_info *thd_info, const class brk_pt &b );
 
 #endif
--- src/trace.cc
+++ src/trace.cc
@@ -7,16 +7,14 @@
 //
 // Redistributions of source code must retain the above copyright notice, this
 // list of conditions and the following disclaimer.
-// Redistributions in binary form must reproduce the above copyright notice,
-// this
+// Redistributions in binary form must reproduce the above copyright notice, this
 // list of conditions and the following disclaimer in the documentation and/or
 // other materials provided with the distribution.
 // Neither the name of The University of British Columbia nor the names of its
 // contributors may be used to endorse or promote products derived from this
 // software without specific prior written permission.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 // DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
@@ -32,27 +30,27 @@
 
 namespace Trace {
 
+
 #define TS_TUP_BEGIN(X) const char* trace_streams_str[] = {
 #define TS_TUP(X) #X
-#define TS_TUP_END(X) \
-  }                   \
-  ;
+#define TS_TUP_END(X) };
 #include "trace_streams.tup"
 #undef TS_TUP_BEGIN
 #undef TS_TUP
 #undef TS_TUP_END
 
-bool enabled = false;
-int sampling_core = 0;
-int sampling_memory_partition = -1;
-bool trace_streams_enabled[NUM_TRACE_STREAMS] = {false};
-const char* config_str;
+    bool enabled = false;
+    int sampling_core = 0;
+    int sampling_memory_partition = -1;
+    bool trace_streams_enabled[NUM_TRACE_STREAMS] = {false};
+    const char* config_str;
 
-void init() {
-  for (unsigned i = 0; i < NUM_TRACE_STREAMS; ++i) {
-    if (strstr(config_str, trace_streams_str[i]) != NULL) {
-      trace_streams_enabled[i] = true;
+    void init()
+    {
+        for ( unsigned i = 0; i < NUM_TRACE_STREAMS; ++i ) {
+            if ( strstr( config_str, trace_streams_str[i] ) != NULL ) {
+                trace_streams_enabled[ i ] = true;
+            }
+        }
     }
-  }
-}
-}
+} 
