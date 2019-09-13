52d863a6e0c78b73cd862b7ec69b9879c64baa58
Nick <coffeebeforearch@gmail.com>
2019-09-13 07:54:00 -0400

    Big reformat change using clang-format-6.0

--- src/gpgpu-sim/traffic_breakdown.cc
+++ src/gpgpu-sim/traffic_breakdown.cc
@@ -1,51 +1,54 @@
-#include "traffic_breakdown.h" 
-#include "mem_fetch.h" 
+#include "traffic_breakdown.h"
+#include "mem_fetch.h"
 
-void traffic_breakdown::print(FILE* fout)
-{
-   for (traffic_stat_t::const_iterator i_stat = m_stats.begin(); i_stat != m_stats.end(); i_stat++) {
-      unsigned int byte_transferred = 0; 
-      for (traffic_class_t::const_iterator i_class = i_stat->second.begin(); i_class != i_stat->second.end(); i_class++) {
-         byte_transferred += i_class->first * i_class->second;  // byte/packet x #packets
-      }
-      fprintf(fout, "traffic_breakdown_%s[%s] = %u {", m_network_name.c_str(), i_stat->first.c_str(), byte_transferred);  
-      for (traffic_class_t::const_iterator i_class = i_stat->second.begin(); i_class != i_stat->second.end(); i_class++) {
-         fprintf(fout, "%u:%u,", i_class->first, i_class->second); 
-      }
-      fprintf(fout, "}\n"); 
-   }
+void traffic_breakdown::print(FILE* fout) {
+  for (traffic_stat_t::const_iterator i_stat = m_stats.begin();
+       i_stat != m_stats.end(); i_stat++) {
+    unsigned int byte_transferred = 0;
+    for (traffic_class_t::const_iterator i_class = i_stat->second.begin();
+         i_class != i_stat->second.end(); i_class++) {
+      byte_transferred +=
+          i_class->first * i_class->second;  // byte/packet x #packets
+    }
+    fprintf(fout, "traffic_breakdown_%s[%s] = %u {", m_network_name.c_str(),
+            i_stat->first.c_str(), byte_transferred);
+    for (traffic_class_t::const_iterator i_class = i_stat->second.begin();
+         i_class != i_stat->second.end(); i_class++) {
+      fprintf(fout, "%u:%u,", i_class->first, i_class->second);
+    }
+    fprintf(fout, "}\n");
+  }
 }
 
-void traffic_breakdown::record_traffic(class mem_fetch * mf, unsigned int size) 
-{
-   m_stats[classify_memfetch(mf)][size] += 1; 
+void traffic_breakdown::record_traffic(class mem_fetch* mf, unsigned int size) {
+  m_stats[classify_memfetch(mf)][size] += 1;
 }
 
-std::string traffic_breakdown::classify_memfetch(class mem_fetch * mf)
-{
-   std::string traffic_name; 
+std::string traffic_breakdown::classify_memfetch(class mem_fetch* mf) {
+  std::string traffic_name;
 
-   enum mem_access_type access_type = mf->get_access_type(); 
+  enum mem_access_type access_type = mf->get_access_type();
 
-   switch (access_type) {
-   case CONST_ACC_R:    
-   case TEXTURE_ACC_R:   
-   case GLOBAL_ACC_W:   
-   case LOCAL_ACC_R:    
-   case LOCAL_ACC_W:    
-   case INST_ACC_R:     
-   case L1_WRBK_ACC:    
-   case L2_WRBK_ACC:    
-   case L1_WR_ALLOC_R:  
-   case L2_WR_ALLOC_R:  
-      traffic_name = mem_access_type_str(access_type); 
-      break; 
-   case GLOBAL_ACC_R:   
-      // check for global atomic operation 
-      traffic_name = (mf->isatomic())? "GLOBAL_ATOMIC" : mem_access_type_str(GLOBAL_ACC_R); 
-      break; 
-   default: assert(0 && "Unknown traffic type"); 
-   }
-   return traffic_name; 
+  switch (access_type) {
+    case CONST_ACC_R:
+    case TEXTURE_ACC_R:
+    case GLOBAL_ACC_W:
+    case LOCAL_ACC_R:
+    case LOCAL_ACC_W:
+    case INST_ACC_R:
+    case L1_WRBK_ACC:
+    case L2_WRBK_ACC:
+    case L1_WR_ALLOC_R:
+    case L2_WR_ALLOC_R:
+      traffic_name = mem_access_type_str(access_type);
+      break;
+    case GLOBAL_ACC_R:
+      // check for global atomic operation
+      traffic_name = (mf->isatomic()) ? "GLOBAL_ATOMIC"
+                                      : mem_access_type_str(GLOBAL_ACC_R);
+      break;
+    default:
+      assert(0 && "Unknown traffic type");
+  }
+  return traffic_name;
 }
-
--- src/option_parser.h
+++ src/option_parser.h
@@ -7,44 +7,44 @@
 //
 // Redistributions of source code must retain the above copyright notice, this
 // list of conditions and the following disclaimer.
-// Redistributions in binary form must reproduce the above copyright notice, this
-// list of conditions and the following disclaimer in the documentation and/or
-// other materials provided with the distribution.
-// Neither the name of The University of British Columbia nor the names of its
-// contributors may be used to endorse or promote products derived from this
-// software without specific prior written permission.
+// Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution. Neither the name of
+// The University of British Columbia nor the names of its contributors may be
+// used to endorse or promote products derived from this software without
+// specific prior written permission.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
-#pragma once 
+#pragma once
 
 #include <stdio.h>
 #include <stdlib.h>
-
 
 // pointer to C++ class
 typedef class OptionParser *option_parser_t;
 
 // data type of the option
 enum option_dtype {
-    OPT_INT32,
-    OPT_UINT32,
-    OPT_INT64,
-    OPT_UINT64,
-    OPT_BOOL,
-    OPT_FLOAT,
-    OPT_DOUBLE,
-    OPT_CHAR,
-    OPT_CSTR
+  OPT_INT32,
+  OPT_UINT32,
+  OPT_INT64,
+  OPT_UINT64,
+  OPT_BOOL,
+  OPT_FLOAT,
+  OPT_DOUBLE,
+  OPT_CHAR,
+  OPT_CSTR
 };
 
 // create and destroy option parser
@@ -52,28 +52,19 @@ option_parser_t option_parser_create();
 void option_parser_destroy(option_parser_t opp);
 
 // register new option
-void option_parser_register(option_parser_t opp, 
-                            const char *name, 
-                            enum option_dtype type, 
-                            void *variable, 
-                            const char *desc,  
-                            const char *defaultvalue);
+void option_parser_register(option_parser_t opp, const char *name,
+                            enum option_dtype type, void *variable,
+                            const char *desc, const char *defaultvalue);
 
 // parse command line
-void option_parser_cmdline(option_parser_t opp,
-                           int argc, const char *argv[]);
-
-
+void option_parser_cmdline(option_parser_t opp, int argc, const char *argv[]);
 
 // parse config file
-void option_parser_cfgfile(option_parser_t opp,
-                           const char *filename);
+void option_parser_cfgfile(option_parser_t opp, const char *filename);
 
 // parse a delimited string
 void option_parser_delimited_string(option_parser_t opp,
-                                    const char *inputstring, 
+                                    const char *inputstring,
                                     const char *delimiters);
 // print options
-void option_parser_print(option_parser_t opp, 
-                         FILE *fout);
-
+void option_parser_print(option_parser_t opp, FILE *fout);
