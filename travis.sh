8ce4e6d3ad69112668e4969db65a71bf7a995dc8
tgrogers <tgrogers@purdue.edu>
2020-07-13 13:05:41 -0400

    updating travis to use the latest infrastrcutre and not use that nasty single-line command line for docker. Also since we don't need torque anymore - none of that runner user stuff is necessary

---
+++ travis.sh
@@ -0,0 +1,24 @@
+if [ ! -n "$CUDA_INSTALL_PATH" ]; then
+	echo "ERROR ** Install CUDA Toolkit and set CUDA_INSTALL_PATH.";
+	exit;
+fi
+
+if [ ! -n "$CONFIG" ]; then
+	echo "ERROR ** set the CONFIG env variable to one of those found in ./accel-sim-framework/util/job_launching/configs/define-standard-cfgs.yml";
+	exit;
+fi
+
+export PATH=$CUDA_INSTALL_PATH/bin:$PATH
+source ./setup_environment
+make -j
+
+rm -rf gpu-app-collection
+git clone https://github.com/accel-sim/gpu-app-collection.git
+source gpu-app-collection/src/setup_environment
+make -j -C gpu-app-collection/src rodinia_2.0-ft
+./gpu-app-collection/get_regression_data.sh
+
+rm -rf accel-sim-framework
+git clone https://github.com/accel-sim/accel-sim-framework.git
+./accel-sim-framework/util/job_launching/run_simulations.py -C $CONFIG -B rodinia_2.0-ft -N regress
+./accel-sim-framework/util/job_launching/monitor_func_test.py -v -N regress
