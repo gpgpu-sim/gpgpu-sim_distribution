8de7987c9cb5fccd9e5c4d2c403d1c4a893a6309
Aditya K Kamath <akamath1997@gmail.com>
2020-09-22 21:50:44 +0530

    Change addresses to 64-bit. Fixes errors with generic addressing

--- src/abstract_hardware_model.h
+++ src/abstract_hardware_model.h
@@ -75,8 +75,8 @@ enum AdaptiveCache { FIXED = 0, ADAPTIVE_VOLTA = 1 };
 
 typedef unsigned long long new_addr_type;
 typedef unsigned long long cudaTextureObject_t;
-typedef unsigned address_type;
-typedef unsigned addr_t;
+typedef unsigned long long address_type;
+typedef unsigned long long addr_t;
 
 // the following are operations the timing model can see
 #define SPECIALIZED_UNIT_NUM 8
