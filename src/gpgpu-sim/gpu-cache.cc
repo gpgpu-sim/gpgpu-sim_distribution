51d99259845a051a32e45763bdf3005b4dff74b5
JRPAN <25518778+JRPan@users.noreply.github.com>
2021-02-15 16:03:51 -0500

    fix hash funciton

--- src/gpgpu-sim/gpu-cache.cc
+++ src/gpgpu-sim/gpu-cache.cc
@@ -63,8 +63,8 @@ unsigned l1d_cache_config::set_bank(new_addr_type addr) const {
   // For sector cache, we select one sector per bank (sector interleaving)
   // This is what was found in Volta (one sector per bank, sector interleaving)
   // otherwise, line interleaving
-  return cache_config::hash_function(addr, l1_banks, l1_banks_byte_interleaving,
-                                     m_l1_banks_log2,
+  return cache_config::hash_function(addr, l1_banks, l1_banks_byte_interleaving_log2,
+                                     l1_banks_log2,
                                      l1_banks_hashing_function);
 }
 
