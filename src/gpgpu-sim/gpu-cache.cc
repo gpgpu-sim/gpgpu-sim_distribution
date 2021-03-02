e05fa4a676c2b082f1ebb34d051f43ad05d4a82c
JRPAN <25518778+JRPan@users.noreply.github.com>
2021-03-02 16:33:30 -0500

    little bug fix - flush()

--- src/gpgpu-sim/gpu-cache.cc
+++ src/gpgpu-sim/gpu-cache.cc
@@ -426,9 +426,10 @@ void tag_array::flush() {
 
   for (unsigned i = 0; i < m_config.get_num_lines(); i++)
     if (m_lines[i]->is_modified_line()) {
-      for (unsigned j = 0; j < SECTOR_CHUNCK_SIZE; j++)
+      for (unsigned j = 0; j < SECTOR_CHUNCK_SIZE; j++) {
         m_lines[i]->set_status(INVALID, mem_access_sector_mask_t().set(j));
         m_dirty--;
+      }
     }
 
   is_used = false;
